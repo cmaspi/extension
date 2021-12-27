f64ce5e1fb9f5cad6ddc7a0e35ca6dcc0724b354
Chirag Mehta <69354325+cmaspi@users.noreply.github.com>
2021-12-27 10:49:00 +0530

    Delete files directory

--- files/captcha1.js
+++
@@ -1,7 +0,0 @@
-function captchaFiller(){
-    str = document.getElementById("appCaptchaLoginImg").src;
-    captcha = str.slice(str.length-5);
-    document.getElementById("captcha").value = captcha;
-}
-
-captchaFiller();
